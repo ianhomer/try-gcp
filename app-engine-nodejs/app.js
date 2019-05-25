494216b4b9d4949a0a2829a66e6718819c9f1502
Ian Homer <ian.homer@gmail.com>
2019-05-25 09:02:36 +0100

    Tweak express app

--- app-engine-nodejs/app.js
+++ app-engine-nodejs/app.js
@@ -13,6 +13,5 @@ app.get('/', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+  console.log(`Server listening on port ${PORT}`);
 });
